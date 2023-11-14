f7d2fbc2bbad74ea83551b642d272e5df7298a94
Aashritha79 <aashritha.dodda79@gmail.com>
2023-11-14 17:34:22 -0600

    files add

---
+++ src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @author Aashritha Dodda- S559361
+ */
+
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+
+/**
+ * 
+ * @author Aashritha Dodda- S559361
+ */
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+	
+	
+
+}
